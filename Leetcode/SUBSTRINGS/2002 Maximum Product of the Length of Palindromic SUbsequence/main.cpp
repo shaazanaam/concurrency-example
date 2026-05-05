83402e303163ea3652f3d84631a5c4893b6ea6dd
Shaaz Anaam <shaazanaam@gmail.com>
2026-05-04 23:25:35 -0500

    adding the bit mask method

--- Leetcode/SUBSTRINGS/2002 Maximum Product of the Length of Palindromic SUbsequence/main.cpp
+++ Leetcode/SUBSTRINGS/2002 Maximum Product of the Length of Palindromic SUbsequence/main.cpp
@@ -2,12 +2,37 @@
 #include<vector>
 #include<string>
 
+
 using namespace std;
 
 class Solution{
       public:
             int maxProduct(string s){
+                  // get n from the string length
+                  // full mask will represent the state where every character in the string  is selected 
+                  //totalMasks one shifted left by n 
+                  int n = s.size();
+                  int totalMask =1<<n;
+                  int fullMask = totalMask-1;
+                  vector<int>pallLen(totalMask,0);
+                  vector<int> palMasks;  // to store only masks that are palindromic
+                  for(int mask =1;mask<=totalMask-1;mask++){
+                       
+                       string subseq;
+                      for (int i = 0; i<n;i++){
+                          if(mask&(1<<i)){  // this is checking if the bit mask is going to include the character at that position 
+                             subseq+= s[i]; 
+                       }
+                      }
+                     
 
+                  }
+                  
+                  return 0;
             }
 
 };
+int main(){
+      Solution sol;
+      cout<<sol.maxProduct("ababa");
+}
